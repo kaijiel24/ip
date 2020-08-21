38fcc6e37baca4a54526e3e11ea40d867568bbb4
kaijiel24 <kaijiel@LAPTOP-43KOVJA4.localdomain>
2020-08-22 00:10:17 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,15 @@
+import java.util.Scanner;
+
 public class Duke {
 
     // Prints a horizontal dash line
     public static void printDashLine(){
         System.out.println("-----------------------------------------------");
+    }
+
+    // Prints the prompt before every user input
+    public static void printPrompt(){
+        System.out.print("  > ");
     }
 
     // Prints the greet statement
@@ -15,6 +22,7 @@ public class Duke {
 
     // Prints the exit statement
     public static void printExitLine(){
+        printDashLine();
         System.out.println("Bye. Hope to see you again soon!");
         printDashLine();
     }
@@ -22,6 +30,23 @@ public class Duke {
     public static void main(String[] args) {
         // Greets user
         printGreeting();
+
+        // Initialise variables to get input from user
+        String line;
+        Scanner in = new Scanner(System.in);
+
+        // Get the first input from user
+        printPrompt();
+        line = in.nextLine();
+
+        // While the input is not "bye", print out the input and get another input
+        while (!line.equals("bye")){
+            printDashLine();
+            System.out.println(line);
+            printDashLine();
+            printPrompt();
+            line = in.nextLine();
+        }
 
         // When the input is bye and we exit from the loop, print exit line
         printExitLine();
