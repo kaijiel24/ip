8c40c932240d5e32542615f9bc2d2021011a35cf
kaijiel24 <kaijiel@LAPTOP-43KOVJA4.localdomain>
2020-08-22 00:04:50 +0800

    Level-0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,29 @@
 public class Duke {
+
+    // Prints a horizontal dash line
+    public static void printDashLine(){
+        System.out.println("-----------------------------------------------");
+    }
+
+    // Prints the greet statement
+    public static void printGreeting(){
+        printDashLine();
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+        printDashLine();
+    }
+
+    // Prints the exit statement
+    public static void printExitLine(){
+        System.out.println("Bye. Hope to see you again soon!");
+        printDashLine();
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        // Greets user
+        printGreeting();
+
+        // When the input is bye and we exit from the loop, print exit line
+        printExitLine();
     }
 }
