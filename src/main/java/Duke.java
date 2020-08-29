9cb4fbfb3cc8342bdfb9daa6cf51f36f0106d347
kaijiel24 <kaijiel@LAPTOP-43KOVJA4.localdomain>
2020-08-29 22:35:48 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,6 +2,13 @@ import java.util.Scanner;
 
 public class Duke {
 
+    public static final String LIST = "list";
+    public static final String BYE = "bye";
+
+
+    public static Task[] taskList= new Task[100];
+    public static int taskCount = 0;
+
     // Prints a horizontal dash line
     public static void printDashLine(){
         System.out.println("-----------------------------------------------");
@@ -27,57 +34,96 @@ public class Duke {
         printDashLine();
     }
 
-    public static void main(String[] args) {
-        // Greets user
-        printGreeting();
+    // Adding a task to list of tasks
+    public static void addTask(String line) {
+        taskList[taskCount] = new Task (line);
+        taskCount = taskCount + 1;
 
+        // Acknowledge input
+        printDashLine();
+        System.out.println("added: " + line);
+        printDashLine();
+    }
+
+    // Print list of task
+    public static void printList() {
+        printDashLine();
+
+        // Print each item
+        for (int i = 0; i < taskCount; ++i) {
+            System.out.print(i + 1 + ".[");
+
+            // If the the index in tasksCompleted is true, then print a tick
+            // else print a cross
+            if (taskList[i].isDone()) {
+                System.out.print("✓");
+            } else {
+                System.out.print("✗");
+            }
+
+            System.out.println("] " + taskList[i].getDescription());
+        }
+
+        printDashLine();
+    }
+
+    public static void markAsDone(int index){
+        // Mark the index as done
+        taskList[index].markAsDone();
+
+        // Acknowledge task is done
+        printDashLine();
+        System.out.println("Nice! I've marked this task as done:");
+        System.out.println("  [✓] " + taskList[index].getDescription());
+        printDashLine();
+    }
+
+    public static void readInput(String line){
+
+        // If input is LIST, print the list of tasks
+        if(line.equals(LIST)) {
+            printList();
+
+        } else if (line.matches("^done \\d+")){
+            // Else if the line is done followed by number
+            // separate the done from the number
+            String[] lineSplit = line.split(" ");
+            int index = Integer.parseInt(lineSplit[1]) - 1;
+
+            markAsDone(index);
+
+        } else {
+            // Else add the input to new list
+            addTask(line);
+        }
+    }
+
+    public static void runDuke(){
         // Initialise variables to get input from user
         String line;
         Scanner in = new Scanner(System.in);
-
-        // Initialize an instance of class Task to keep track of tasks
-        Task taskList = new Task();
 
         // Get the first input from user
         printPrompt();
         line = in.nextLine();
 
         // While the input is not "bye", add input to array of tasks and get another input
-        while (!line.equals("bye")){
+        while (!line.equals(BYE)){
 
-            // If input is "list", print the list of tasks
-            if(line.equals("list")) {
-                printDashLine();
-                taskList.printList();
-                printDashLine();
-            } else if (line.matches("^done \\d+")){
-                // Else if the line is done followed by number
-                // separate the done from the number
-                String[] lineSplit = line.split(" ");
-                int index = Integer.parseInt(lineSplit[1]);
-
-                // Mark the index as done
-                taskList.markAsDone(index);
-
-                // Acknowledge task is done
-                printDashLine();
-                System.out.println("Nice! I've marked this task as done:");
-                System.out.println("  [✓] " + taskList.getTask(index));
-                printDashLine();
-            } else {
-                // Else add the input to new list
-                taskList.addTask(line);
-
-                // Acknowledge input
-                printDashLine();
-                System.out.println("added: " + line);
-                printDashLine();
-            }
+            readInput(line);
 
             // Get another input
             printPrompt();
             line = in.nextLine();
         }
+    }
+
+    public static void main(String[] args) {
+        // Greets user
+        printGreeting();
+
+        // Run Duke until it exits
+        runDuke();
 
         // When the input is bye and we exit from the loop, print exit line
         printExitLine();
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,55 +1,22 @@
 public class Task {
-    private String[] tasks;
-    private boolean[] tasksCompleted;
-    private int taskCount;
+    private final String description;
+    private boolean isDone;
 
-    public Task(){
-        this.tasks = new String[100];
-        this.tasksCompleted = new boolean[100];
-        taskCount = 0;
+    public Task(String description){
+        this.description = description;
+        this.isDone = false;
     }
 
-    // Get the number of tasks
-    public int getTaskCount(){
-        return taskCount;
+    public String getDescription() {
+        return description;
     }
 
-    // Adding a task to list of tasks
-    public void addTask(String line){
-        // Add line to array of tasks, set newly added index in tasksCompleted as false
-        // and increase the task count
-        tasks[taskCount] = line;
-        tasksCompleted[taskCount] = false;
-        taskCount = taskCount + 1;
-    }
-
-    // Get the task based on the index
-    public String getTask(int index){
-        return tasks[index-1];
+    public boolean isDone() {
+        return this.isDone;
     }
 
     // Mark a task as done based on the index
-    public void markAsDone(int index){
-        // Mark the index as tasksCompleted
-        tasksCompleted[index-1] = true;
-    }
-
-    // Print the list of tasks
-    public void printList(){
-
-        // Print each item
-        for (int i = 0; i < taskCount; ++i){
-            System.out.print(i+1 + ".[");
-
-            // If the the index in tasksCompleted is true, then print a tick
-            // else print a cross
-            if (tasksCompleted[i]){
-                System.out.print("✓");
-            } else {
-                System.out.print("✗");
-            }
-
-            System.out.println("] " + tasks[i]);
-        }
+    public void markAsDone(){
+        isDone = true;
     }
 }
